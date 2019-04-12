a973ac4b1186f75a7ef00030f4aac4240a62c805
rohithkk <rohithkodakandla@gmail.com>
2019-04-11 23:41:22 -0400

    changed main script name

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 const geocode = require('../utils/geocode.js');
 const forecast = require('../utils/forecast.js');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //current directory
 console.log(__dirname);
